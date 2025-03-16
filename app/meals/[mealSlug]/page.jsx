977a2fecd446418ae8c34b1a1c01d3f03668a26e
strawhatsaad <strawhatsaad123@gmail.com>
2025-03-16 06:02:18 +0500

    fix: Update image path in MealPage component
    This commit fixes an issue in the `MealPage` component where the image path was incorrect. The `Image` component's `src` attribute is updated to include `/public` before the meal's image path. This ensures that the images are correctly loaded and displayed.

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -25,7 +25,7 @@ export default async function MealPage({ params }) {
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image src={meal.image} alt={meal.title} fill />
+          <Image src={`/public${meal.image}`} alt={meal.title} fill />
         </div>
         <div className={classes.headerText}>
           <h1>{meal.title}</h1>
